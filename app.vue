08461ee4f38062a6e8463249f4850694443dd181
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-07-18 15:36:24 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="teste">
+    Ola mundo
+    <span>Oi</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .teste {
+    color: red;
+
+    span {
+      color: blue;
+    }
+  }
+</style>
