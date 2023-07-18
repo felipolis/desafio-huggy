b73fb7ab29a3c976b130e14a769da739f15bf338
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-07-18 19:43:24 -0300

    Adicionando rota privada

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+    import { useUserStore } from '../stores/token';
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+    const userStore = useUserStore();
+
+    onMounted(() => {
+        if (!userStore.token.access_token) {
+            router.push('/login');
+        }
+    })
+</script>
+
 <template>
     <div>Chats</div>
 </template>
--- stores/token.js
+++ stores/token.js
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('token', () => {
-    const token = useLocalStorage('token', {
+    const token = ref({
         token_type: 'Bearer',
         expires_in: 0,
         access_token: '',
@@ -10,10 +10,7 @@ export const useUserStore = defineStore('token', () => {
     })
 
     const setToken = (data) => {
-        token.value.token_type = data.token_type
-        token.value.expires_in = data.expires_in
-        token.value.access_token = data.access_token
-        token.value.refresh_token = data.refresh_token
+        token.value = data
     }
 
     return {
