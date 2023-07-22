099a782c966e0dee601624b5456121edd4f781f8
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-07-22 20:38:44 -0300

    Criando o componente do header

---
+++ components/H3ader.vue
@@ -0,0 +1,52 @@
+<script setup>
+    import { useUserStore } from '../stores/user';
+
+    const userSore = useUserStore()
+
+</script>
+
+<template>
+    <div class="headerSection">
+        <div class="avatar">
+            <img :src="userSore.user.avatar" alt="avatar" />
+        </div>
+        <div class="contactName">{{ userSore.user.name }}</div>
+    </div>
+</template>
+
+<style lang="scss">
+    @import "@/assets/scss/global.scss";
+    .headerSection {
+        width: 510px;
+        height: 64px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        padding-right: 16px;
+        padding-left: 16px;
+        background-color: $fill-neutral-low-0;
+        gap: 8px;
+
+        .avatar {
+            width: 40px;
+            height: 40px;
+            border-radius: 8px;
+            overflow: hidden;
+            border: 1px solid $border-neutral-1;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+
+        .contactName {
+            font-size: 16px;
+            font-weight: 600;
+            color: $text-low-strong;
+            font-size: $body-1;
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -119,13 +119,7 @@
             <div class="mensagens">
                 <div v-if="userSore.user.name !== ''">
                     <!-- HEADER -->
-                    <div class="headerSection">
-                        <div class="avatar">
-                            <img :src="userSore.user.avatar" alt="avatar" />
-                        </div>
-                        <div class="contactName">{{ userSore.user.name }}</div>
-                    </div>
-
+                    <H3ader />
 
 
                 </div>
@@ -164,39 +158,7 @@
         display: flex;
         flex-direction: column;
 
-        .headerSection {
-            width: 510px;
-            height: 64px;
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-start;
-            align-items: center;
-            padding-right: 16px;
-            padding-left: 16px;
-            background-color: $fill-neutral-low-0;
-            gap: 8px;
-
-            .avatar {
-                width: 40px;
-                height: 40px;
-                border-radius: 8px;
-                overflow: hidden;
-                border: 1px solid $border-neutral-1;
-
-                img {
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                }
-            }
-
-            .contactName {
-                font-size: 16px;
-                font-weight: 600;
-                color: $text-low-strong;
-                font-size: $body-1;
-            }
-        }
+        
 
     }
 </style>
